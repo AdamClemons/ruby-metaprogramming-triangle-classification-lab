4c9bcec93a8d5b414f28529698c8c5d216efd827
Adam Clemons <74882592+thisClemons@users.noreply.github.com>
2021-10-17 12:52:52 -0400

    commit tree 75f3f330b1f474ae2a19586cdfdc1f8eb73a28a9

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
+require 'pry'
+!!!
 class Triangle
   # write code here
+
+  # attr_reader :s1, :s2, :s3
+  attr_reader :sides
+
+  def initialize(s1, s2, s3)
+    @sides = [s1, s2, s3]
+  end
+
+  def kind
+    sides.sort!
+    # binding.pry
+    if sides[0] + sides[1] <= sides[2] || sides[0] <= 0
+      # begin
+        raise TriangleError
+      # rescue TriangleError => error
+      #   puts error.message
+      # end
+    elsif sides[0] == sides[2]
+      :equilateral
+    elsif sides[1] == sides[2]
+      :isosceles
+    else
+      :scalene
+    end
+
+  end
+
+  class TriangleError < StandardError
+    
+  end
+
 end
